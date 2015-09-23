af0f91babbc1f5a57c839491bf33dddf52c26737
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2015-09-23 14:41:26 +0200

    Changed download-java.bat to use outdated java version that works on windows server 2003.

--- src/main/assembly/jdk/download-java.bat
+++ src/main/assembly/jdk/download-java.bat
@@ -1,18 +1,22 @@
 @echo off
-set ZipFile=jdk.zip
+set DOWNLOAD_URL="http://mvnrepo.cantara.no/service/local/artifact/maven/redirect?r=nmdreleases&g=net.java.jdk8&a=jre&v=LATEST&p=zip"
+set TmpDir=.
+set ZipFile=jre.zip
 set BaseDir=.
-::wget.exe -e use_proxy=yes -e http_proxy=10.246.5.10:8080 -P . -O jre.zip --referer=http://www.azulsystems.com/products/zulu/downloads http://cdn.azulsystems.com/zulu/2015-04-8.7-bin/zulu1.8.0_45-8.7.0.5-win64.zip
-wget.exe -P . -O %ZipFile%  --referer=http://www.azulsystems.com/products/zulu/downloads http://cdn.azulsystems.com/zulu/2015-04-8.7-bin/zulu1.8.0_45-8.7.0.5-win64.zip
+wget.exe --user=altran --password=l1nkSys -e use_proxy=yes -e http_proxy=10.246.5.10:8080 -P "%TmpDir%" -O %ZipFile% --content-disposition %DOWNLOAD_URL%
+::wget.exe --user=altran --password=l1nkSys -e use_proxy=no -e http_proxy=10.246.5.10:8080 -P "%TmpDir%" -O %ZipFile% --content-disposition %DOWNLOAD_URL%
 
-unzip.exe -q -o %ZipFile% 
-move zulu1.8.0_45-8.7.0.5-win64 java
+unzip -q -o %TmpDir%\%ZipFile% -d %BaseDir%\java
+if exist %BaseDir%\java\jre1.8.0_40 (
+    echo "Copy java files %BaseDir%\java\bin"
+    move %BaseDir%\java\jre1.8.0_40\*.* %BaseDir%\java\
+	move %BaseDir%\java\jre1.8.0_40\bin %BaseDir%\java\bin
+	move %BaseDir%\java\jre1.8.0_40\lib %BaseDir%\java\lib
+	copy %BaseDir%\java\bin\client %BaseDir%\java\bin\server
+)
 
 :: Copy windows login dll
 if exist %BaseDir%\java\bin (
     echo "Copy windows login dll %BaseDir%\java\bin"
     copy %BaseDir%\ntlmauth.dll %BaseDir%\java\bin
 )
-
-del %ZipFile%
-echo "Copy windows login dll %BaseDir%\java\bin"
-echo "zulu1.8.0_45-8.7.0.5-win64 JDK downloaded to java folder"
