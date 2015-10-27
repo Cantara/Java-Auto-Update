0efb0b2b136a528c9bc132523f4b2cc2be51d874
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2015-10-27 16:07:37 +0100

    Added some tests in RegisterClientExceptionHandlerTest.

--- src/test/java/no/cantara/jau/coms/RegisterClientExceptionHandlerTest.java
+++ src/test/java/no/cantara/jau/coms/RegisterClientExceptionHandlerTest.java
@@ -1,12 +1,69 @@
 package no.cantara.jau.coms;
 
+import com.netflix.hystrix.exception.HystrixRuntimeException;
+import org.springframework.util.backoff.BackOffExecution;
+import static org.mockito.Mockito.*;
 import org.testng.annotations.Test;
+
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.NotFoundException;
+import java.net.ConnectException;
+import java.util.concurrent.TimeoutException;
 
 public class RegisterClientExceptionHandlerTest {
 
     @Test
-    public void testShouldRetryAccordingly() {
+    public void testShouldHandleConnectExceptionWithRetry() {
+        BackOffExecution backOffExecution = mock(BackOffExecution.class);
+        when(backOffExecution.nextBackOff()).thenReturn((long) 1);
 
+        HystrixRuntimeException hystrixRuntimeException = mock(HystrixRuntimeException.class);
+        when(hystrixRuntimeException.getCause()).thenReturn(new ConnectException());
+
+        RegisterClientExceptionHandler.handleRegisterClientException(hystrixRuntimeException, null, backOffExecution, "");
+
+        verify(backOffExecution).nextBackOff();
     }
+
+    @Test
+    public void testShouldHandleInternalServerErrorExceptionWithRetry() {
+        BackOffExecution backOffExecution = mock(BackOffExecution.class);
+        when(backOffExecution.nextBackOff()).thenReturn((long) 1);
+
+        HystrixRuntimeException hystrixRuntimeException = mock(HystrixRuntimeException.class);
+        when(hystrixRuntimeException.getCause()).thenReturn(new InternalServerErrorException());
+
+        RegisterClientExceptionHandler.handleRegisterClientException(hystrixRuntimeException, null, backOffExecution, "");
+
+        verify(backOffExecution).nextBackOff();
+    }
+
+    @Test
+     public void testShouldHandleNotFoundExceptionWithRetry() {
+        BackOffExecution backOffExecution = mock(BackOffExecution.class);
+        when(backOffExecution.nextBackOff()).thenReturn((long) 1);
+
+        HystrixRuntimeException hystrixRuntimeException = mock(HystrixRuntimeException.class);
+        when(hystrixRuntimeException.getCause()).thenReturn(new NotFoundException());
+
+        RegisterClientExceptionHandler.handleRegisterClientException(hystrixRuntimeException, null, backOffExecution, "");
+
+        verify(backOffExecution).nextBackOff();
+    }
+
+    @Test
+    public void testShouldHandleTimeoutExceptionWithRetry() {
+        BackOffExecution backOffExecution = mock(BackOffExecution.class);
+        when(backOffExecution.nextBackOff()).thenReturn((long) 1);
+
+        HystrixRuntimeException hystrixRuntimeException = mock(HystrixRuntimeException.class);
+        when(hystrixRuntimeException.getCause()).thenReturn(new TimeoutException());
+
+        RegisterClientExceptionHandler.handleRegisterClientException(hystrixRuntimeException, null, backOffExecution, "");
+
+        verify(backOffExecution).nextBackOff();
+    }
+
+
 
 }
