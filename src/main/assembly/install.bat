c09140eec5f01e8ab6580bf6bdf91a30b7f774fc
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2015-10-28 15:27:37 +0100

    Added removing of old agents and old jau to install.bat.

--- src/main/assembly/install.bat
+++ src/main/assembly/install.bat
@@ -1,5 +1,18 @@
 @echo off
 
+:: Remove very old agent if present
+if exist C:\NMD\avbagent\pharmacy-agent\bin\pharmacy-agent (
+	C:\NMD\avbagent\pharmacy-agent\bin\pharmacy-agent remove
+	taskkill /im java.exe /F
+)
+
+:: If previous version of java-auto-update and agent is running remove them
+if exist c:\opt\java-auto-update\bin\java-auto-update (
+	c:\opt\java-auto-update\bin\java-auto-update remove
+	taskkill /im java.exe /F
+)
+
+
 :: Download java if java folder is missing
 if not exist java (
     call C:\opt\java-auto-update\download-java.bat
