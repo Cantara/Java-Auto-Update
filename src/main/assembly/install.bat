35da407c2d4df0a37fda9cf4d4bd9d8ad436218f
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2015-10-29 08:20:31 +0100

    Shortend call to download-java.bat.

--- src/main/assembly/install.bat
+++ src/main/assembly/install.bat
@@ -17,7 +17,7 @@ if exist bin\java-auto-update (
 
 :: Download java if java folder is missing
 if not exist java (
-    call C:\opt\java-auto-update\download-java.bat
+    call download-java.bat
 )
 
 bin\java-auto-update remove & bin\java-auto-update install & bin\java-auto-update start & sc failure java-auto-update reset= 3000  actions= restart/30000/restart/90000/restart/300000
