286ea5270c78d63846460df89b3b1373dede37ef
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2015-10-29 13:16:58 +0100

    Added else conditions with echoing for more info.

--- src/main/assembly/jdk/uninstallAndRemoveAnyOldVersion.bat
+++ src/main/assembly/jdk/uninstallAndRemoveAnyOldVersion.bat
@@ -5,6 +5,8 @@ if exist C:\NMD\avbagent\pharmacy-agent\bin\pharmacy-agent (
 	C:\NMD\avbagent\pharmacy-agent\bin\pharmacy-agent remove
 	taskkill /im java.exe /F
 	echo Removing very old Pharmacy Agent
+) else (
+    echo Didn't find very old Pharmacy Agent, skipping it.
 )
 
 :: If previous version of java-auto-update and agent is running remove them and remove directory
@@ -15,4 +17,6 @@ if exist c:\opt\java-auto-update\bin\java-auto-update (
 
 	echo Deleting c:\opt\java-auto-update
 	RMDIR /S /Q c:\opt\java-auto-update
+) else (
+    echo Didnt find folder: c:\opt\java-auto-update\bin\java-auto-update . Skipping uninstall of it.
 )
