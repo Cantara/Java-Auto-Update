589ecf4e3c1ac9b247ecdad8731271a49a34cfc1
Anders Emil Rønning <andeero@ifi.uio.no>
2015-11-10 09:44:22 +0100

    Remove unused imports

--- src/test/java/no/cantara/jau/processkill/DuplicateProcessHandlerIntTest.java
+++ src/test/java/no/cantara/jau/processkill/DuplicateProcessHandlerIntTest.java
@@ -1,6 +1,5 @@
 package no.cantara.jau.processkill;
 
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -10,8 +9,6 @@ import java.io.*;
 import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
-import static org.mockito.Mockito.*;
 
 public class DuplicateProcessHandlerIntTest {
     private static final Logger log = LoggerFactory.getLogger(DuplicateProcessHandlerIntTest.class);
