3043a214a5707d635510cf1d3374acccfc8bc9be
Anders Emil Rønning <andeero@ifi.uio.no>
2016-01-07 11:28:58 +0100

    Instantiate map in EventRepo. THIS should fix the NPE

--- src/main/java/no/cantara/jau/eventextraction/EventRepo.java
+++ src/main/java/no/cantara/jau/eventextraction/EventRepo.java
@@ -1,9 +1,15 @@
 package no.cantara.jau.eventextraction;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class EventRepo {
     List<NumberedLine> events;
+
+    public EventRepo() {
+        this.events = new LinkedList<>();
+    }
 
     public void addEvents(List<NumberedLine> eventsToAdd) {
         events.addAll(eventsToAdd);
