4603b57ff4fa78388ac9e0c98d888011e372499f
Anders Emil Rønning <andeero@ifi.uio.no>
2016-01-15 12:50:59 +0100

    Rename file

--- src/main/java/no/cantara/jau/eventextraction/dto/ExtractedEventsStore.java
+++ src/main/java/no/cantara/jau/eventextraction/dto/ExtractedEventsStore.java
@@ -5,10 +5,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class ExtractedEvents {
+public class ExtractedEventsStore {
     private final Map<String, EventGroup> eventGroups;
 
-    public ExtractedEvents() {
+    public ExtractedEventsStore() {
         eventGroups = new HashMap<>();
     }
 
