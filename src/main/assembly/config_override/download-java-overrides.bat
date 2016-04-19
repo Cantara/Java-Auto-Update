384d397ba26c5b357d54083317405164e275d9ba
Sindre Mehus <sm@scienta.no>
2016-04-19 10:37:28 +0200

    Support installation of Java crypto extension.

--- src/main/assembly/config_override/download-java-overrides.bat
+++ src/main/assembly/config_override/download-java-overrides.bat
@@ -4,9 +4,22 @@
 :: Overrides properties in download-java.bat
 ::
 
-::SET DOWNLOAD_URL=
-::SET DOWNLOAD_URL_32_BIT=
-::SET ZIP_ROOT_DIR=
-::SET HTTP_PROXY=
-::SET HTTP_USER=
-::SET HTTP_PASSWORD=
+:: Download URL for the "normal" 64-bit JDK
+:: SET DOWNLOAD_URL=
+
+:: Download URL for the 32-bit JDK that will be downloaded on Windows 2003 servers
+:: SET DOWNLOAD_URL_32_BIT=
+
+:: Download URL for the optional cryptography extension kit, e.g.,
+:: http://www.azulsystems.com/sites/default/files/images/ZuluJCEPolicies.zip
+:: SET DOWNLOAD_URL_CRYPTO=
+
+:: Root directory in the downloaded zip file.  Only relevant for the 64-bit JDK.
+:: SET ZIP_ROOT_DIR=
+
+:: Set this if using an http proxy. Specified as "host:port".
+:: SET HTTP_PROXY=
+
+:: Username and password for HTTP authentication. Only relevant for the 32-bit JDK.
+:: SET HTTP_USER=
+:: SET HTTP_PASSWORD=
--- src/main/assembly/jdk/download-java.bat
+++ src/main/assembly/jdk/download-java.bat
@@ -10,11 +10,12 @@ SET DOWNLOAD_URL="http://cdn.azul.com/zulu/bin/zulu8.13.0.5-jdk8.0.72-win_x64.zi
 :: Download URL for the 32-bit JDK that will be downloaded on Windows 2003 servers
 SET DOWNLOAD_URL_32_BIT=
 
+:: Download URL for the optional cryptography extension kit, e.g.,
+:: http://www.azulsystems.com/sites/default/files/images/ZuluJCEPolicies.zip
+SET DOWNLOAD_URL_CRYPTO=
+
 :: Root directory in the downloaded zip file.  Only relevant for the 64-bit JDK.
 SET ZIP_ROOT_DIR="zulu8.13.0.5-jdk8.0.72-win_x64"
-
-:: Name of the temporary zip file.
-SET ZIP_FILE="java.zip"
 
 :: Set this if using an http proxy. Specified as "host:port".
 SET HTTP_PROXY=
@@ -23,6 +24,9 @@ SET HTTP_PROXY=
 SET HTTP_USER=
 SET HTTP_PASSWORD=
 
+
+SET ZIP_FILE="java.zip"
+SET ZIP_FILE_CRYPTO="crypto.zip"
 
 CALL config_override\download-java-overrides.bat
 
@@ -55,7 +59,7 @@ if exist java\jre1.8.0_40 (
     xcopy /E /O /Y java\bin\client\* java\bin\server\
 )
  
-GOTO finish
+GOTO crypto
  
  
  
@@ -72,9 +76,26 @@ echo Unzipping %ZIP_FILE%
 unzip.exe -q -o %ZIP_FILE%
 move %ZIP_ROOT_DIR% java
 
+GOTO crypto
+ 
+
+
+:crypto
+IF defined DOWNLOAD_URL_CRYPTO (
+    echo Downloading crypto extension from %DOWNLOAD_URL_CRYPTO%
+    IF defined HTTP_PROXY (
+        wget.exe -e use_proxy=yes -e http_proxy=%HTTP_PROXY% -O %ZIP_FILE_CRYPTO% %DOWNLOAD_URL_CRYPTO%
+    ) ELSE (
+        wget.exe -O %ZIP_FILE_CRYPTO% %DOWNLOAD_URL_CRYPTO%
+    )
+    echo Unzipping %ZIP_FILE_CRYPTO%
+    unzip.exe -o -j %ZIP_FILE_CRYPTO% *.jar -d java/jre/lib/security
+)
+
 GOTO finish
- 
- 
+
+
+
  
 :finish
 
@@ -86,4 +107,7 @@ if exist java\bin (
 
 if exist %ZIP_FILE% (
     del %ZIP_FILE%
+)
+if exist %ZIP_FILE_CRYPTO% (
+    del %ZIP_FILE_CRYPTO%
 )
