6f36e120d9d5915c8a90f4118c95e9e7885f6e95
Sindre Mehus <sm@scienta.no>
2016-06-07 13:35:07 +0200

    Support overriding properties in etc/wrapper.conf

---
+++ src/main/assembly/environment-setup.bat
@@ -0,0 +1,12 @@
+
+rem   Placeholder file for overriding properties in etc/wrapper.conf
+rem
+rem   Set the WRAPPER_CONF_OVERRIDES environment variable to override wrapper properties.
+rem   For example, to override the Windows service name, uncomment the following line:
+rem
+rem   set WRAPPER_CONF_OVERRIDES=wrapper.ntservice.name=myagent wrapper.ntservice.displayname=myagent
+rem
+rem   For more details, see:
+rem
+rem   http://www.mojohaus.org/appassembler/appassembler-maven-plugin/generate-daemons-mojo.html#environmentSetupFileName
+rem   http://wrapper.tanukisoftware.com/doc/english/props-command-line.html
